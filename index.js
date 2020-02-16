e4ffc6c7c8ac7bfc4bbdb0a7f69771d33447ac78
Anffer Castillo <anffer.castillo@gfs.com>
2020-02-16 12:52:02 -0300

    full chat

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-let connectedUsers = {};
+const connectedUsers = {};
+const chatRooms = {};
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,7 +17,6 @@ io.on('connection', (socket) => {
     io.emit('user-connected', { users: connectedUsers })
 
     socket.on('disconnect', () => {
-        console.log(`user ${socket.id} is gone`);
         let user = '';
         for(const username in connectedUsers){
             if(connectedUsers[username] === socket.id){
@@ -28,13 +28,32 @@ io.on('connection', (socket) => {
     });
 
     socket.on('invite', (data) => {
-        console.log(data);
         socket.to(connectedUsers[data.invited]).emit('user-invitation', { user: data.inviter });
-    })
+    });
 
-    socket.on('message', (data) => {
-        console.log(data.message);
-    })
+    socket.on('invitation-accepted', (data, fn) => {
+        const room = new Date().getTime();
+        chatRooms[room] = {
+            user1: data.invited,
+            user2: data.inviter
+        };
+        socket.join(room);
+        socket.to(connectedUsers[data.inviter]).emit('invitation-answer-yes', {room: room});
+        fn(room);
+    });
+
+    socket.on('invitation-rejected', (data) => {
+        socket.to(connectedUsers[data.inviter]).emit('invitation-answer-no');
+    });
+
+    socket.on('join-room', (data) => {
+        socket.join(data.room);
+    });
+
+    socket.on('message', (data, fn) => {
+        socket.to(connectedUsers[data.to]).emit('message', data);
+        fn(data);
+    });
 });
 
 app.get('/',  (req, res) => {
--- public/js/chat-client.js
+++ public/js/chat-client.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const _user = prompt('Enter your username');
     let _guest = '';
     let _myVideo = {};
@@ -8,16 +8,23 @@
     let _inviteChatBtn = {};
     let _startVideoBtn = {};
     let _stopVideoBtn = {};
-    let _send = {};
+    let _sendBtn = {};
     let _socket = undefined;
-    let _connectedUsers = {}
+    let _connectedUsers = {};
+    let _acceptBtn = {};
+    let _rejectBtn = {};
+    let _invitationText = {};
+    let _usernameLabel =  $('#username-label');
+    let _guestnameLabel = $('#guestname-label');
+    let _room = '';
+    let _chatRoomLabel = $('#chat-room');
 
-    if(_user !== null) {
+    if (_user !== null) {
         _initChatClient();
     }
 
     function _initChatClient() {
-        $('#username-1').html(_user);
+        _usernameLabel.html(_user);
         _getElementReferences();
         _socket = io(`http://localhost:3000/?user=${_user}`);
         _setupSocketEnvets();
@@ -32,55 +39,99 @@
         _inviteChatBtn = $('#invite-chat');
         _connectedUsersBox = $('#connected-users-box');
         _chatBox = $('#chat-box');
-        _send = $('#send-btn');
+        _chatInput = $('#chat-input');
+        _sendBtn = $('#send-btn');
+        _acceptBtn = $('#accept-btn');
+        _rejectBtn = $('#reject-btn');
+        $('#invitation-modal').modal({ show: false});
+        _invitationText = $('#invitation-text');
     }
 
     function _sendInvite(event) {
-        console.log(event);
-        _socket.emit('invite', { invited: _guest.data('guest'), inviter: _user })
+        _socket.emit('invite', { invited: _guest, inviter: _user });
+    }
+
+    function _joinRoom(room) {
+        _room = room;
+        _socket.emit('join-room', { room: room });
+        _chatRoomLabel.html(room);
+    }
+
+    function _answerInvite(event) {
+        if (event.target.id == 'accept-btn') {
+            _socket.emit('invitation-accepted', { invited: _user, inviter: _guest }, (room) => _chatRoomLabel.html(room));
+            _guestnameLabel.html(_guest);
+        } else {
+            _socket.emit('invitation-rejected', { invited: _user, inviter: _guest });
+        }
+        $('#invitation-modal').modal('hide');
+    }
+
+    function _sendMessage(){
+        if(!!_chatInput.val()) {
+            _socket.emit('message', {to: _guest, message: _chatInput.val() }, _handleMessage)
+            _chatInput.val('');
+        }
     }
 
     function _setupUIEvents() {
         _inviteChatBtn.click(_sendInvite);
+        _acceptBtn.click(_answerInvite);
+        _rejectBtn.click(_answerInvite);
+        _sendBtn.click(_sendMessage);
     }
 
 
-    function _handleMessage(data) {
-        const messageContainer = $(`<div class="alert alert-secondary" role="alert">${data.message}</div>`)
+    function _handleMessage({to, message}) {
+        const messageContainer = $(`<div class="alert ${to != _user ? 'alert-primary text-right' :'alert-secondary'}" role="alert">${message}</div>`)
         _chatBox.append(messageContainer);
     }
 
     function _handleUserConnection(data) {
         for (const user in data.users) {
-            if(!!_connectedUsers[user] == false && user !== _user) {
+            if (!!_connectedUsers[user] == false && user !== _user) {
                 _addToConnectedUserBox(user);
-                _connectedUsers[user] = data.users[user];
+                _connectedUsers[user] = user;
             }
         }
     }
 
-    function _addToConnectedUserBox(user){
+    function _addToConnectedUserBox(user) {
         const userContainer = $(`<div data-guest="${user}" class="connected-user alert alert-secondary" role="alert">${user}</div>`);
         userContainer.click((e) => {
-            _guest = $(e.target)
             _connectedUsersBox.find('.connected-user').addClass('alert-secondary').removeClass('alert-primary');
-            _guest.removeClass('alert-secondary').addClass('alert-primary');
+            $(e.target).removeClass('alert-secondary').addClass('alert-primary');
+            _guest = user;
         });
         _connectedUsersBox.append(userContainer);
     }
 
     function _handleUserDisconnection(data) {
         _connectedUsersBox.find(`[data-guest="${data.user}"]`).remove();
+        delete _connectedUsers[data.user];
     }
 
     function _handleUserInvitation(data) {
-        console.log(data);
+        _invitationText.html(`${data.user} quiere chatear contigo`);
+        $('#invitation-modal').modal('show');
+        _guest = data.user;
     }
 
-    function _setupSocketEnvets(){
+    function _handleInvitationAnswerYes(data){
+        _guestnameLabel.html(_guest);
+        _joinRoom(data.room);
+    }
+
+    function _handleInvitationAnswerNo(data){
+        alert(`${_guest} rechazo tu unvitacion`);
+    }
+
+    function _setupSocketEnvets() {
         _socket.on('message', _handleMessage);
         _socket.on('user-connected', _handleUserConnection);
         _socket.on('user-disconnect', _handleUserDisconnection);
         _socket.on('user-invitation', _handleUserInvitation);
+        _socket.on('invitation-answer-yes', _handleInvitationAnswerYes);
+        _socket.on('invitation-answer-no', _handleInvitationAnswerNo);
     }
 })(io, $) 
